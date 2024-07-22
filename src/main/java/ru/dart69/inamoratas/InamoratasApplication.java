086c5c93624e9c23a5844778be6312cb5ec4f726
dart69 <dart69@vk.com>
2024-07-22 13:18:56 +0300

    An initial commit after creating the project using Spring initializr

---
+++ src/main/java/ru/dart69/inamoratas/InamoratasApplication.java
@@ -0,0 +1,13 @@
+package ru.dart69.inamoratas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InamoratasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InamoratasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dart69/inamoratas/InamoratasApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dart69.inamoratas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InamoratasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
